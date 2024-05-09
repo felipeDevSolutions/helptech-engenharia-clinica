dfcce78ff9ef7f672f2727eb6432398762d1f36d
Felipe Costa <felipeejazz@gmail.com>
2024-05-09 15:57:16 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// header scrolling effect
+$(window).on('scroll', function(){
+	if($(window).scrollTop()){
+      $('header').addClass('nav-show');
+		  
+	} 
+	else{
+		$('header').removeClass('nav-show');
+	}
+	   
+})
+
+//hamburger
+const navSlide = () => {
+	 const hamburger = document.querySelector(".hamburger");
+	 const navbar = document.querySelector(".nav-bar");
+	 const navLinks = document.querySelectorAll(".nav-bar li");
+
+     hamburger.onclick = () => {
+		
+	 navbar.classList.toggle("nav-active");
+		 
+      //Animation links
+	 navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = "";
+		} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7+1}s`;
+		   }
+		});
+	  //hamburger animation
+	 hamburger.classList.toggle("toggle");
+    }
+	 
+	}
+
+window.onload = () => navSlide();
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+function openFluxogramaModal(imgSrc) {
+    var modal = document.getElementById('fluxogramaModal');
+    var modalImg = document.getElementById('fluxogramaImg');
+    modalImg.src = imgSrc;
+    modal.style.display = "block";
+}
+
